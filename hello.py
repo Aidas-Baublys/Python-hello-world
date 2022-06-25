9da7c90ee0ac0530f183d4259e1902c5dc967a33
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-06-25 20:28:04 +0300

    Learning numpy indexing

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-a = np.array([1, 2])
+a = np.random.randint(0, 11, (6, 6))
 
-b = np.repeat(a, 2)
-
-print(b)
+print(a)
+print(a[2:4, 0:2])
+print(a[[0, 1, 2, 3], [2, 3, 4, 5]])
+print(a[[0, -2, -1], 4:])
