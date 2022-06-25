d924e69a93b995449505e468fe94a261771c8daf
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-06-25 18:44:03 +0300

    Learning numpy

--- hello.py
+++ hello.py
@@ -1,18 +1,5 @@
-def average(*args):
-    print(sum(args) / len(args))
+import numpy as np
 
+a = np.ones((2, 2))
 
-average(1, 2, 3, 4, 5, 6)
-
-
-def average_2(*args):
-    res = 0
-    counter = 0
-    for arg in args:
-        res += arg
-        counter += 1
-
-    print(res / counter)
-
-
-average_2(1, 2, 3, 4, 5, 6)
+print(a)
