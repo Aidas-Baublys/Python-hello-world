620823747827ad9bae3811301a1562bde1becff1
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-06-26 16:11:35 +0300

    Learning numpy

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
 import numpy as np
 
-a = np.random.randint(0, 11, (6, 6))
 
-print(a)
-print(a[2:4, 0:2])
-print(a[[0, 1, 2, 3], [2, 3, 4, 5]])
-print(a[[0, -2, -1], 4:])
+a = np.random.rand(3, 3, 3)
+
+print(a.ndim)
