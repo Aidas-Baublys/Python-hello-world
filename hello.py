93bbef2cde5b7828b81b4062d3711a8b9feae20a
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-07-12 11:47:06 +0300

    Add csv chunks and chunking logic

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 import pandas as pd
+
+path_to_csv = "./csv/survey_results_public.csv"
+
+
+for i, chunk in enumerate(pd.read_csv(path_to_csv, chunksize=40000)):
+    chunk.to_csv(f"./csv_chunks/chunk{i}.csv", index=False)
